6650ebf663667170b46ef1173efc2009042a9c5f
maicaocaa <maria.caocaa@gmail.com>
2024-02-12 16:58:01 +0100

    create migrations tables ok

---
+++ database/migrations/2024_02_12_152326_resources_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('link');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('id_type');
+            $table->foreign('id_type')->references('id')->on('types')->onDelete('cascade');
+            $table->unsignedBigInteger('id_tag');
+            $table->foreign('id_tag')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2024_02_12_152444_extras_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('extras', function (Blueprint $table) {
+            $table->id();
+            $table->string('extra_name');
+            $table->string('extra_link');
+            $table->unsignedBigInteger('id_tag');
+            $table->foreign('id_tag')->references('id')->on('tags')->onDelete('cascade');
+            $table->unsignedBigInteger('id_resource');
+            $table->foreign('id_resource')->references('id')->on('resources')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
