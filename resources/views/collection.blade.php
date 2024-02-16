f04a390e51eb72f4cb08c33dd221f781b5f8f531
Gabriela Farinha <vallefarinha@gmail.com>
2024-02-16 07:59:56 -0300

    add order by date of creation

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -10,47 +10,56 @@
 
 <body>
     @extends('navbar')
-
     @section('view')
-    <table id="collection" class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Tag</th>
-                <th scope="col">Type</th>
-                <th scope="col">Title</th>
-                <th scope="col">Number of resources</th>
-                <th scope="col">Date of creation</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($collections as $collection)
-            <tr>
-                <td>
-                    <a href="{{ route('resource.resource', ['resource' => $collection->id]) }}">
-                        {{ $collection->tag->tag_name }}
-                    </a>
-                </td>
-                <td> <a href="{{ route('resource.resource', ['resource' => $collection->id]) }}">
-                        {{ $collection->type->type_name }}
-                    </a>
-                </td>
-                <td>{{ $collection->title }}</td>
-                <td>{{ $collection->countTotalExtras() }}</td>
-                <td>{{ $collection->created_at }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="container pt-3">
+        <table id="collection" class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Tag</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Number of resources</th>
+                    <th scope="col">Date of creation</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($collections as $collection)
+                <tr>
+                    <td>
+                        <a href="{{ route('resource.resource', ['resource' => $collection->id]) }}">
+                            {{ $collection->tag->tag_name }}
+                        </a>
+                    </td>
+                    <td> <a href="{{ route('resource.resource', ['resource' => $collection->id]) }}">
+                            {{ $collection->type->type_name }}
+                        </a>
+                    </td>
+                    <td>{{ $collection->title }}</td>
+                    <td>{{ $collection->countTotalExtras() }}</td>
+                    <td>{{ $collection->created_at }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+
     @endsection
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
     <script>
     $(document).ready(function() {
-        $('#collection').DataTable();
+        $('#collection').DataTable({
+            "order": [
+                [4, "desc"]
+            ]
+        });
     });
     </script>
 </body>
+
+
 
 
 
